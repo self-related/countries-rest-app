a966a6d9a6e00c2da13111d5e23a533b50b386af
Nekit <nick.misiura@yandex.ru>
2025-05-07 16:01:38 +0300

    Style App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import styles from "./App.module.scss";
 
 function App() {
 
   return (
-    <div>
-        <main>
+    <div className={styles.app} >
+        <main className={styles.main} >
             <Outlet />
         </main>
     </div>
