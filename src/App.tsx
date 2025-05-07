047ed02ff5ebe8ae9740468ac4dec15255ec707c
Nekit <nick.misiura@yandex.ru>
2025-05-07 15:50:26 +0300

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+        <main>
+            <Outlet />
+        </main>
+    </div>
   )
 }
 
---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+export const router = createBrowserRouter([
+    {
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <p>Temp</p>
+            }
+        ]
+    }
+]);
