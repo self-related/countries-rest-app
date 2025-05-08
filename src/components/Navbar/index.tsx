ad2d644d49c1984a42874103988cbb0b150de402
Nekit <nick.misiura@yandex.ru>
2025-05-08 16:51:56 +0300

    Add home button on navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,15 @@
 import styles from "./styles.module.scss";
+import HomeButton from "../../assets/home_icon_1.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+    const navigate = useNavigate();
 
     return (
         <nav className={styles.nav}>
+            <button className={styles.homeButton} onClick={() => navigate("/")}>
+                <img src={HomeButton} alt="Home Button" />
+            </button>
             <p>Language:</p>
             <select name="language" id="language">
                 <option value="temp">Temp</option>
