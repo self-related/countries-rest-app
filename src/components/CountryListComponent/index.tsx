6cf151d484dd1eab2d56315914e1560edeea57a1
Nekit <nick.misiura@yandex.ru>
2025-05-08 15:49:41 +0300

    Add sort by population

--- src/components/CountryListComponent/index.tsx
+++ src/components/CountryListComponent/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import type { Country } from "../../redux/features/api/types";
 import styles from "./styles.module.scss";
+import { useEffect, useState } from "react";
 
 interface CountryListComponentProps {
     countries: Country[] | undefined
@@ -8,11 +9,32 @@ interface CountryListComponentProps {
 
 export default function CountryListComponent({ countries }: CountryListComponentProps) {
   const navigate = useNavigate();
+  const [countriesList, setCountriesList] = useState(countries);
+
+
+
+  const handleSortByPopulation = () => {
+    if (!countriesList) {
+        return;
+    }
+    
+    const countriesListSorted = [...countriesList].sort((countryA, countryB) => countryB.population - countryA.population);
+    setCountriesList(countriesListSorted);
+  };
+
+  // change country list if props changed
+  useEffect(() => {
+    setCountriesList(countries);
+  }, [countries]);
+
   return (
     <div className={styles.countryList}>
+        <div>
+            <button onClick={handleSortByPopulation} className={styles.sortByPopulation}>By population</button>
+        </div>
         {
-            countries != null
-            ? countries?.map((country, index) => (
+            countriesList != null
+            ? countriesList?.map((country, index) => (
                 <div className={styles.country} key={`country-${index}`} onClick={() => navigate(`/country/${country.cca3}`)}>
                     <p>{country.flag} {country.name.common}</p>
                 </div>
