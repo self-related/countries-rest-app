9ddc04402dac04fbad4ea0a457665855b9764342
Nekit <nick.misiura@yandex.ru>
2025-05-08 12:05:14 +0300

    Add fetching data on search page depending on params

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,16 +1,25 @@
 import styles from "./styles.module.scss";
 import { useParams } from "react-router-dom";
 import SearchIcon from "../../assets/search_icon.svg";
+import { useFetchCountriesQuery } from "../../redux/features/api/restCountriesApiSlice";
 
 export default function SearchPage() {
+    const params = useParams<{query: string}>();
+    console.log("params: ", params);
 
-console.log(useParams());
+    const skip: boolean = params.query == null;
+    const {data} = useFetchCountriesQuery(params.query!, { skip });
+
+    console.log("data:", data);
 
     return (
         <div className={styles.searchPage}>
             <div className={styles.searchPanel}>
                 <input type="text" />
                 <button><img src={SearchIcon} alt="search" /></button>
+            </div>
+            <div className={styles.searchResults}>
+
             </div>
 
         </div>
