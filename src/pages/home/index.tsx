cdaceba1df2f76e575b4bdc1af3c63a13aad406c
Nekit <nick.misiura@yandex.ru>
2025-05-08 15:08:02 +0300

    Set navigation on home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.scss";
 
 export default function Home() {
+    const navigate = useNavigate();
 
     return (
         <div className={styles.homePage}>
             <h1>REST Countries App</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
             <div className={styles.buttons}>
-                <button className={styles.greenButton}>All Countries</button>
-                <button className={styles.blueButton}>Search</button>
+                <button className={styles.greenButton} onClick={() => navigate("/search/all")}>All Countries</button>
+                <button className={styles.blueButton} onClick={() => navigate("/search")}>Search</button>
             </div>
         </div>
     );
