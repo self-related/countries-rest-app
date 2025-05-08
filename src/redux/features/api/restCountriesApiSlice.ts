b5e6ab19499fbd815d7895c97f8abf7b60396b57
Nekit <nick.misiura@yandex.ru>
2025-05-08 12:38:45 +0300

    Define Country type and respond type for restCountriesSlice

--- src/redux/features/api/restCountriesApiSlice.ts
+++ src/redux/features/api/restCountriesApiSlice.ts
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { FetchCountriesRespond } from "./types";
 
 const baseUrl = "https://restcountries.com/v3.1";
 
 export const restCountriesApiSlice = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (build) => ({
-        fetchCountries: build.query({
+        fetchCountries: build.query<FetchCountriesRespond, string>({
             query: (query: string) => {
                 if (query == "all") {
                     return {
---
+++ src/redux/features/api/types.ts
@@ -0,0 +1,23 @@
+type CCA_3 = string;
+type urlString = string;
+
+export interface Country {
+    name: string,
+    region: string,
+    borders: CCA_3[], // cca3 codes array
+    cca3: CCA_3, // string
+    translations: {
+        [tr: string]: {
+            official: string,
+            common: string,
+        }
+    },
+    flag: string,
+    flags: {
+        png: urlString,
+        svg: urlString,
+        alt: string, // text
+    }
+}
+
+export type FetchCountriesRespond = Country[];
